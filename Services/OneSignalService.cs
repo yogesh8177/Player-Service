9c0ffa5ec4dd6d6b38e8c46e2496ffa69101868f
Yogesh <yogesh.jadhav8177@gmail.com>
2019-07-27 14:04:43 +0530

    OneSignal service class updated

--- Services/OneSignalService.cs
+++ Services/OneSignalService.cs
@@ -1,18 +1,19 @@
 using OneSignal.AspNet.Core.SDK;
 using OneSignal.AspNet.Core.SDK.Resources.Notifications;
 using System.Collections.Generic;
+using System;
 
 namespace Player_Service.Services {
     public class OneSignalService {
         private readonly OneSignalClient client;
 
         public OneSignalService () {
-            client = new OneSignalClient("");
+            client = new OneSignalClient(Environment.GetEnvironmentVariable("ONESIGNAL_API_KEY"));
         }
 
         public NotificationCreateResult createNotification (List<string> TargetSegments, string message) {
             var options = new NotificationCreateOptions();
-            options.AppId = System.Guid.Parse("");
+            options.AppId = System.Guid.Parse(Environment.GetEnvironmentVariable("ONESIGNAL_APP_ID"));
             options.IncludedSegments = TargetSegments;
             options.Contents.Add("Message", message);
 
