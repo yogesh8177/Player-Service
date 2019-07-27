46f5e7dfb1075a888b962b8bec1206a9b008d8fe
Yogesh <yogesh.jadhav8177@gmail.com>
2019-07-27 14:02:12 +0530

    sendbird service class updated

--- Services/SendBirdService.cs
+++ Services/SendBirdService.cs
@@ -1,12 +1,28 @@
 using SendBird;
 using WebSocketSharp;
 using System.Collections.Generic;
+using System;
 
 
 namespace Player_Service.Services {
     public class SendBirdService {
         public SendBirdService () {
+            SendBirdClient.Init(Environment.GetEnvironmentVariable("SENDBIRD_APP_ID"));
+        }
 
+        public void sendSystemChatMessage (string userId, string channelUrl, string message) {
+            SendBirdClient.Connect(userId, (User user, SendBirdException connectException) => {
+                // get channel
+                GroupChannel.GetChannel(channelUrl,
+                    (GroupChannel groupChannel, SendBirdException getChannelException) => {
+                    // send message
+                    groupChannel.SendUserMessage(message,
+                        (UserMessage userMessage, SendBirdException sendMessageException) => {
+                        // message sent
+                        });
+                });
+            });
         }
     }
+    
 }
